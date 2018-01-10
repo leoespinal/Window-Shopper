e3f9742f2944f8b7288b83c47663c3585622abe6
Leo Espinal <leo.espinal420@gmail.com>
2018-01-09 22:56:37 -0500

    final version 1.0 of app

---
+++ Window-Shopper/Model/Wage.swift
@@ -0,0 +1,9 @@
+//
+//  Wage.swift
+//  Window-Shopper
+//
+//  Created by Leo Espinal on 1/9/18.
+//  Copyright © 2018 Leo Espinal. All rights reserved.
+//
+
+import Foundation
---
+++ Window-Shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTextField.swift
+//  Window-Shopper
+//
+//  Created by Leo Espinal on 1/8/18.
+//  Copyright © 2018 Leo Espinal. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
