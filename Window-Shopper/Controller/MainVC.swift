7285b3f7c1a0027e76fe72e65bb3f37af4d6bf99
Leo Espinal <leo.espinal420@gmail.com>
2018-01-09 22:58:59 -0500

    working version of app

--- Window-Shopper/Controller/MainVC.swift
+++ Window-Shopper/Controller/MainVC.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addCalculateButtonToView()
+        resultsLabel.isHidden = true
+        hoursLabel.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func clearCalculatorPressed(_ sender: Any) {
+        resultsLabel.isHidden = true
+        hoursLabel.isHidden = true
+        wageTxt.text = ""
+        priceTxt.text = ""
     }
-
+    
+    @objc func calculate() {
+        //Make sure fields arent empty
+        if let wageTxt = wageTxt.text, let priceTxt = priceTxt.text {
+            //Make sure input values in text field are Doubles
+            if let wage = Double(wageTxt), let price = Double(priceTxt) {
+                view.endEditing(true)
+                resultsLabel.isHidden = false
+                hoursLabel.isHidden = false
+                //Set results label text to number of hours it takes to purchase item
+                resultsLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+                
+            }
+        }
+    }
+    
+    func addCalculateButtonToView() {
+        //Create the Calculate button and attach it to the text field as an accessory
+        let calButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calButton.backgroundColor = #colorLiteral(red: 1, green: 0.7563767854, blue: 0.2894808301, alpha: 1)
+        calButton.setTitle("Calculate", for: .normal)
+        calButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        //Set the accessory views for the text fields
+        wageTxt.inputAccessoryView = calButton
+        priceTxt.inputAccessoryView = calButton
+    }
 
 }
 
--- Window-Shopper/Views/CurrencyTextField.swift
+++ Window-Shopper/Views/CurrencyTextField.swift
@@ -8,14 +8,56 @@
 
 import UIKit
 
+//Have changes in this class show in interface builder using @IBDesignable
+@IBDesignable
 class CurrencyTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
+    //Don't implement this function if it's empty. Use when you want to add a subview to your view
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let size: CGFloat = 20
+        //Use UI components with frames when creating a UI object programatically
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.7161303889, green: 0.7161303889, blue: 0.7161303889, alpha: 0.8)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.2901960784, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        //Set the clip to bounds property when working in the draw( rect) method for UI elements that need to be clipped
+        currencyLabel.clipsToBounds = true
+        
+        //Create a NumberFormatter which will be use to create a currency symbol in the view which is customize based on the location where a device is from
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        //Set the symbol from the formatter to the currency label
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
     }
-    */
+    
+    //Method called when interface builder is going to be loaded
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    //When the view is loaded, this method will be called at runtime
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        //Set attributes for the text field
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        clipsToBounds = true
+        
+        //Modify placeholder text if not nil
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 
 }
